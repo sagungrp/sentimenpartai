416bc58b8af28bee9b795468785950be5f62b84a
kDasa <kabul.dasa@ui.ac.id>
2018-12-14 21:19:10 +0700

    clean up code

--- coba.py
+++
@@ -1,3 +0,0 @@
-import re
-
-print(re.sub(r'([a-z])\1+', r'\1', 'aakuuu pulaaannggg'))
--- sentimen_partai_backend/sentimen_search/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SentimenSearchConfig(AppConfig):
-    name = 'sentimen_search'
